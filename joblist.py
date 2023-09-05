e1679699f2ad032edfad1e21137d6e77f9325167
Celvis-wq <celvis.hacks@gmail.com>
2023-09-05 10:37:11 -0500

    first commit

---
+++ joblist.py
@@ -0,0 +1,15 @@
+# joblist.py
+
+a = "String 1"
+b = "String 2"
+
+x = "String 3"
+y = "String 4"
+print(f"x = {x} and y = {y}")
+
+print("c = {c} and d = {d}".format(c=1, d=2))
+
+jobList = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, i in jobList.items():
+    print(f"{name} is a {i}")
